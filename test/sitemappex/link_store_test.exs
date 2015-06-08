59cefb79f1db859c41c0a7e23afbaaec8579382e
Sean Callan <seancallan@gmail.com>
2015-06-07 20:21:02 -0700

    Create our initial LinkStore

---
+++ test/sitemappex/link_store_test.exs
@@ -0,0 +1,18 @@
+defmodule Sitemappex.LinkStoreTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    {:ok, link_store} = Sitemappex.LinkStore.start_link
+    {:ok, link_store: link_store}
+  end
+
+  test "responds to new and existing correctly", %{link_store: link_store} do
+    assert :new = Sitemappex.LinkStore.add(link_store, "http://example.org")
+    assert {:existing, _} = Sitemappex.LinkStore.add(link_store, "http://example.org")
+  end
+
+  test "responds to to_list", %{link_store: link_store} do
+    Sitemappex.LinkStore.add(link_store, "http://example.org")
+    assert [{_, 1}] = Sitemappex.LinkStore.to_list(link_store)
+  end
+end
