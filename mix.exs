5c7ea3cac1f11030fd0ab6f2a74ecc5177f53d19
Sean Callan <seancallan@gmail.com>
2015-06-11 20:33:29 -0700

    Include mapping functionality

--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule Sitemappex.Mixfile do
     [app: :sitemappex,
      version: "0.0.1",
      elixir: "~> 1.0",
+     escript: [main_module: Sitemappex],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -14,7 +15,7 @@ defmodule Sitemappex.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +28,10 @@ defmodule Sitemappex.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:sorted_set, "~> 1.0"}]
+    [
+      {:exvcr, "~> 0.4"},
+      {:floki, "~> 0.3"},
+      {:httpoison, "~> 0.7"}
+    ]
   end
 end
--- test/sitemappex_test.exs
+++ test/sitemappex_test.exs
@@ -1,7 +1,18 @@
 defmodule SitemappexTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup_all do
+    ExVCR.Config.cassette_library_dir("fixture/vcr_cassettes", "test/fixture/custom_cassettes")
+    :ok
+  end
+
+  @tag timeout: 1000
+
+  test "with valid response" do
+    use_cassette "response_mocking", custom: true do
+      result = Sitemappex.map_links("http://example.com")
+      assert 3 = length(result)
+    end
   end
 end
